54ce9694b04d29cec4534523dd1f868ba03f2263
alladikrishnapriya <alladikrishnapriyaa@gmail.com>
2026-09-20 12:18:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
+# Define two numbers using variables
+a = 10
+<<<<<<< HEAD
+b = 32
+=======
+b = 30
+>>>>>>> 49f3b8a08fde8142dfcfdea6bc9ae43b322be1e9
 
+# Basic operations
+addition = a + b        # 13
+subtraction = a - b     # 7
+multiplication = a * b  # 30
+division = a / b        # 3.3333333333333335 (Always returns a decimal)
